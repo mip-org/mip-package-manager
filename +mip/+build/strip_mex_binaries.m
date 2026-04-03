c273184409eaf5046186cf9ad4ee4315fe44314e
Jeremy Magland <jmagland@flatironinstitute.org>
2026-04-03 12:02:11 -0400

    Only show MEX binary stripping message when binaries exist
    Fixes #43

--- +mip/+build/strip_mex_binaries.m
+++ +mip/+build/strip_mex_binaries.m
@@ -1,12 +1,16 @@
-function strip_mex_binaries(dirPath)
+function count = strip_mex_binaries(dirPath)
 %STRIP_MEX_BINARIES   Remove pre-existing MEX binaries from a directory tree.
 %
 % Args:
 %   dirPath - Directory to recursively scan and clean
+%
+% Returns:
+%   count - Number of MEX binaries removed
 
 mexExtensions = {'.mexa64', '.mexmaci64', '.mexmaca64', '.mexw64', ...
                  '.mexw32', '.mexglx', '.mexmac'};
 
+count = 0;
 items = dir(fullfile(dirPath, '**'));
 for i = 1:length(items)
     if items(i).isdir
@@ -17,6 +21,7 @@ for i = 1:length(items)
             filePath = fullfile(items(i).folder, items(i).name);
             delete(filePath);
             fprintf('  Removed mex binary: %s\n', items(i).name);
+            count = count + 1;
             break;
         end
     end
