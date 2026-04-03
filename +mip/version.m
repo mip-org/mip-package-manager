c062335a8439375f36b421cbeb1f7a3328cb779a
Jeremy Magland <jmagland@flatironinstitute.org>
2026-04-03 12:19:14 -0400

    Error instead of falling back to 'dev' when mip.json is missing

--- +mip/version.m
+++ +mip/version.m
@@ -4,14 +4,14 @@ function v = version()
 thisDir = fileparts(mfilename('fullpath'));  % +mip directory
 pkgRoot = fileparts(thisDir);  % package root
 mipJsonPath = fullfile(pkgRoot, 'mip.json');
-if exist(mipJsonPath, 'file')
-    pkgInfo = mip.utils.read_package_json(pkgRoot);
-    v = pkgInfo.version;
-    if isnumeric(v)
-        v = num2str(v);
-    end
-else
-    v = 'dev';
+if ~exist(mipJsonPath, 'file')
+    error('mip:version:noMipJson', ...
+          'mip.json not found at %s. Is mip installed correctly?', pkgRoot);
+end
+pkgInfo = mip.utils.read_package_json(pkgRoot);
+v = pkgInfo.version;
+if isnumeric(v)
+    v = num2str(v);
 end
 
 end
